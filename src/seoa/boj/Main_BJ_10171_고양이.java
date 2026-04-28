7ff4d3e46e5abb2cd9091a5393915997c030ec53
jayakeie <sjkkls0711@gmail.com>
2026-04-28 15:42:02 +0900

    [소서아] BOJ_10171_고양이, 10250_ACM호텔, 1152_단어의 개수, 2884_알람시계, 2920_음계

---
+++ src/seoa/boj/Main_BJ_10171_고양이.java
@@ -0,0 +1,10 @@
+package seoa.boj;
+
+public class Main_BJ_10171_고양이 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
