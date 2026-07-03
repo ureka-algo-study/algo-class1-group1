d2e64784aa9c41d7a02a676f25b837e0f65ec517
SLug801 <jw7581171@gmail.com>
2026-07-03 11:45:07 +0900

    Partition Labels 문제풀이 추가

--- jaewoo/week7/L_Longest Palindromic Substring.java
+++ jaewoo/week7/L_Longest Palindromic Substring.java
@@ -1,15 +1,15 @@
 class Solution {
     public String longestPalindrome(String s) {
         int start = 0;
-        int end = 0;
+        int end = 0; // 마지막 계산식에서 사용
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) { // 홀수와 짝수 
             int len1 = expand(s, i, i);
             int len2 = expand(s, i, i + 1);
 
-            int len = Math.max(len1, len2);
+            int len = Math.max(len1, len2); // 둘중 더 긴거
 
-            if (len > end - start + 1) {
+            if (len > end - start + 1) {  
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
             }
