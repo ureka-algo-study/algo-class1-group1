db7b29952e3a6daf65460a351ea2388c9e2c8522
jayakeie <sjkkls0711@gmail.com>
2026-07-09 17:23:17 +0900

    [소서아] LeetCode_309_Best Time to Buy and Sell Stock with Cooldown

---
+++ seoa/leetcode/LeetCode_309_BestTimetoBuyandSellStockwithCooldown.java
@@ -0,0 +1,23 @@
+package seoa.LeetCode;
+
+public class LeetCode_309_BestTimetoBuyandSellStockwithCooldown {
+	public int maxProfit(int[] prices) {
+		if (prices == null || prices.length <= 1) return 0;
+		
+		int hold = -prices[0];
+		int sold = 0;
+		int rest = 0;
+		
+		for (int i = 1; i < prices.length; i++) {
+			int prevHold = hold;
+			int prevSold = sold;
+			int prevRest = rest;
+	    
+			hold = Math.max(prevHold, prevRest - prices[i]);
+			sold = prevHold + prices[i];
+			rest = Math.max(prevRest, prevSold);
+			}
+		
+		return Math.max(sold, rest);
+	}
+}
